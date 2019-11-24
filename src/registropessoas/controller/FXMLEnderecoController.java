7e11b03edb9be9384c8295ba0396ee758da07852
Darci Neto <netoschneideer@gmail.com>
2019-11-24 11:53:47 -0300

    Criação da interface de Endereços e adição dos controllers.

--- src/registropessoas/controller/FXMLEnderecoController.java
+++ src/registropessoas/controller/FXMLEnderecoController.java
@@ -7,7 +7,10 @@ package registropessoas.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 /**
  * FXML Controller class
@@ -15,7 +18,31 @@ import javafx.fxml.Initializable;
  * @author neto_
  */
 public class FXMLEnderecoController implements Initializable {
+    
+    @FXML
+    private TableColumn<?, ?> clm_logradouro_tbv_enderecos;
 
+    @FXML
+    private TableColumn<?, ?> clm_pais_tbv_enderecos;
+
+    @FXML
+    private TableColumn<?, ?> clm_cep_tbv_enderecos;
+
+    @FXML
+    private TableView<?> tbv_enderecos;
+
+    @FXML
+    private TableColumn<?, ?> clm_estado_tbv_enderecos;
+
+    @FXML
+    private TableColumn<?, ?> clm_codigo_tbv_enderecos;
+
+    @FXML
+    private TableColumn<?, ?> clm_numero_tbv_enderecos;
+
+    @FXML
+    private TableColumn<?, ?> clm_bairro_tbv_enderecos;
+    
     /**
      * Initializes the controller class.
      */
